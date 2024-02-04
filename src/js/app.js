6f79ba26abf9ff08271be7fc4c721b9d2818da65
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2024-02-04 23:25:16 +0200

    Fixed JS

--- src/js/app.js
+++ src/js/app.js
@@ -7,19 +7,32 @@ const form = document.querySelector(".contact-us__form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const name = form.querySelector("#form-name").value;
-    const phone = form.querySelector("#form-phone").value;
-    const message = form.querySelector("#form-textarea").value;
+    const name = form.querySelector("#form-name");
+    const phone = form.querySelector("#form-phone");
+    const message = form.querySelector("#form-textarea");
 
-    const res_message = `Ім'я: ${name}%0AНомер: ${phone}%0AПовідомлення: ${message}`;
+    const res_message = `Ім'я: ${name.value}%0AНомер: ${phone.value}%0AПовідомлення: ${message.value}`;
 
     const token = "6500254603:AAElY-Vf796qx9FmZCPHAdmD3ffnm20rN6o";
     const chat_id = -4190749445;
-    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${res_message}/`;
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${res_message}`;
 
-    let api = new XMLHttpRequest();
-    api.open("GET", url, true);
-    api.send();
+    async function fetchUsers(url_address) {
+        return await fetch(url_address).then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+    }
 
-    console.log("Successfully sended!");
+    fetchUsers(url)
+        .then(() => {
+            console.log("Successfully sended!");
+            // Очищення значень полів форми
+            name.value = "";
+            phone.value = "";
+            message.value = "";
+        })
+        .catch((error) => console.log(error));
 });
