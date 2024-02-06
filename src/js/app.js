d41364a08f260e94899aed43b99f7deca8e8663e
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2024-02-06 19:47:32 +0200

    Header adaptive, burger-menu done, favicons added

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as helpFunctions from "./modules/functions.js";
 import formFoo from "./modules/form.js";
 import scrollToAnchors from "./modules/scroll-to-anchors.js";
+import mobileMenu from "./modules/mobile-menu.js";
 
 helpFunctions.isWebp();
 formFoo();
 scrollToAnchors();
+mobileMenu();
