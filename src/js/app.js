2d5d289f8e358b10b6c500d6babe10581a8668a0
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2024-02-13 21:27:40 +0200

    small fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
-import * as helpFunctions from "./modules/functions.js";
 import formFoo from "./modules/form.js";
 import scrollToAnchors from "./modules/scroll-to-anchors.js";
 import mobileMenu from "./modules/mobile-menu.js";
 import modalWindow from "./modules/modal.js";
 
-helpFunctions.isWebp();
 formFoo();
 scrollToAnchors();
 mobileMenu();
